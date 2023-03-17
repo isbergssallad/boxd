1a4d31b8cc6eedc20468449bc31d3a8379f7c08a
isbergssallad <116280808+isbergssallad@users.noreply.github.com>
2023-03-17 20:58:29 +0100

    3
    2023-03-17
    20:58

---
+++ movie-list.js
@@ -0,0 +1,18 @@
+window.movies = [
+    {
+        id: "aftersun",
+        name: "Aftersun",
+        year: "(2022)",    
+    },
+    {
+        id: "everything-everywhere-all-at-once",
+        name: "Everything Everywhere All At Once",
+        year: "(2022)",
+    },
+    {
+        id: "heat",
+        name: "Heat",
+        year: "(1995)",
+    },
+
+]
