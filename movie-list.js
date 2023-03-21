12db195b1f02e28311589f7f7e83c5d2b865dd46
isbergssallad <116280808+isbergssallad@users.noreply.github.com>
2023-03-21 11:28:55 +0100

    4
    2023-03-21
    11:28

--- movie-list.js
+++ movie-list.js
@@ -2,17 +2,30 @@ window.movies = [
     {
         id: "aftersun",
         name: "Aftersun",
-        year: "(2022)",    
+        year: "(2022)",  
+        trailer: "",
+        rated: "5",
     },
     {
         id: "everything-everywhere-all-at-once",
         name: "Everything Everywhere All At Once",
         year: "(2022)",
+        trailer: "",
+        rated: "5",
     },
     {
         id: "heat",
         name: "Heat",
         year: "(1995)",
+        trailer: "",
+        rated: "5",
     },
+    {
+        id: "minari",
+        name: "Minari",
+        year: "(2020)",
+        trailer: "",
+        rated: "5",
+    }
 
 ]
