ea0d452a79c07f2e95391e8cdf31eb0f431dcd44
isbergssallad <116280808+isbergssallad@users.noreply.github.com>
2023-04-26 14:56:17 +0200

    31
    2023-04-26
    14:56

--- movie-list.js
+++ movie-list.js
@@ -133,7 +133,7 @@ window.movies = [
         year: "(1994)",
         director: "Frank Darabont",
         trailer: "https://youtu.be/PLl99DlL6b4",
-        rated: 4,
+        rated: 3,
         runtime: "142",
         tagline: "Fear can hold you prisoner. Hope can set you free.",
         description: "Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
